775475bae7808f0b25e28b8ed5672c13d045fbda
Talon Hughes <talon.hughes@gmail.com>
2016-09-10 13:57:38 -0600

    answers so far

--- arrayReview.js
+++ arrayReview.js
@@ -1,20 +1,30 @@
-var threeItems = [1,2,3];
+var threeItems = [1, 2, 3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
-
-
+//code here
+function last(threeItems) {
+    return threeItems[threeItems.length - 1];
+}
+console.log(threeItems);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Loop through evenArray removing all values that aren't even 
-var evenArray = [1,2,3,6,22,98,45,23,22,12];
+//Loop through evenArray removing all values that aren't even
+var evenArray = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
 
-  //code here
-
+//code here
+function findEvens(evenArray) {
+    var newArray = [];
+    evenArray.forEach(function(item) {
+        if (item % 2 === 0) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+}
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -22,25 +32,37 @@ var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
 //below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
-  return Math.floor(Math.random() * (30 - 0) + 0);
+    return Math.floor(Math.random() * (30 - 0) + 0);
+};
+var randomArray = [0, 3, 4, 5, 6, 7, 9, 14, 17, 24, 25, 26, 29, 30];
+
+//code here
+function numFinder(arr) {
+    var random = getRandomArbitrary();
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === random) {
+            return true;
+        }
+    }
+    return false;
 }
-var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
-
-  //code here
-
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
 //Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
-var first = [1,2,3,4,5];
+var first = [1, 2, 3, 4, 5];
 var second;
 
-  //code here
-
-alert(first) //[1,2,3,4,5];
-alert(second) //[1,2,3,4,5,6,7];
+//code here
+var second = [];
+for (var i = 0; i < first.length; i++) {
+    second[i] = first[i];
+}
+second.push(6, 7);
+alert(first); //[1,2,3,4,5];
+alert(second); //[1,2,3,4,5,6,7];
 
 
 
@@ -49,22 +71,40 @@ alert(second) //[1,2,3,4,5,6,7];
 
 
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
-var sentence = "Dev Mountain is the best"
+var sentence = "Dev Mountain is the best";
 
-  //code here
-
-
+//code here
+function longest(str) {
+    var strSplit = str.split(' ');
+    var longestWord = 0;
+    var word = null;
+    strSplit.forEach(function(strSplit) {
+        if (longestWord < strSplit.length) {
+            longestWord = strSplit.length;
+            word = strSplit;
+        }
+    });
+    return word;
+}
+console.log(longest("Dev Mountain is the best"));
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//write a function called capitalize that takes in the myPoem variable and capitalizes every word 
-var myPoem = 'What is a jQuery but a misunderstood object?'
+//write a function called capitalize that takes in the myPoem variable and capitalizes every word
+var myPoem = 'What is a jQuery but a misunderstood object?';
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
-
-
+//code here
+function capitalize(str) {
+    var words = str.split(' ');
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i].split('');
+        word[0] = word[0].toUpperCase();
+        words[i] = word.join('');
+    }
+    return splitStr.join(' ');
+}
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -72,3 +112,12 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+function vowelCounter(str) {
+    var vowels = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === 'a' || str.charAt(i) === 'e' || str.charAt(i) === 'i' || str.charAt(i) === 'o' || str.charAt(i) === 'u') {
+            vowels++;
+        }
+    }
+    return vowels;
+}
--- callbackReview.js
+++ callbackReview.js
@@ -1,17 +1,21 @@
 /* Declare and Define the functions here that will make the function calls below work properly */
-
+function first(arr, cb){
+  cb(arr[0]);
+}
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
-  console.log('The first name in names is ', firstName)
+  console.log('The first name in names is ', firstName);
 });
 
 
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
+function last(arr, cb){
+  cb(arr[arr.length - 1]);
+}
 
 
 
@@ -26,7 +30,15 @@ last(names, function(lastName){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 //have the contains function return a boolean value for if the name is in the array or not.
-
+function contains(arr, str, cb){
+  var exists = false;
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] === str){
+      exists = true;
+    }
+  }
+  cb(exists);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains('Colt', names, function(yes){
@@ -41,9 +53,13 @@ contains('Colt', names, function(yes){
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
-
-
+function map(arr, cb) {
+  var newNums = arr.slice(0, arr.length);
+  for(var i = 0; i < newNums.length; i++) {
+    newNums[i] = cb(newNums[i]);
+  }
+  return newNums;
+}
 
 var numbers = [1,2,3,4,5];
 //Produces a new array of values by mapping each value in list through a transformation function
@@ -55,9 +71,18 @@ map(numbers, function(num){
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
-
-
+function uniq(arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+      for (var x = 0; x < arr.length; x++) {
+        if (i !== x) {
+          if (arr[i] === arr[x]) {
+          arr.splice(x, 1);
+          }
+        }
+      }
+    }
+      cb(arr);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
@@ -68,57 +93,69 @@ uniq(names, function(uniqArr){
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
-
+function each(arr, cb){
+  for(var i = 0; i < arr.length; i++){
+    cb(arr[i], i);
+  }
+}
 
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + 'position is ' + item);
 });
 
 
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+function getUserById(arr, str, cb){
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i].id === str){
+      return cb(arr[i]);
+    }
+  }
+}
 
 
 
-
-var users = [
-  {
+var users = [{
     id: '12d',
     email: 'tyler@gmail.com',
     name: 'Tyler',
     address: '167 East 500 North'
-  },
-  {
+  },{
     id: '15a',
     email: 'cahlan@gmail.com',
     name: 'Cahlan',
     address: '135 East 320 North'
-  },
-  {
+  },{
     id: '16t',
     email: 'ryan@gmail.com',
     name: 'Ryan',
     address: '192 East 32 North'
-  },
-];
+  },];
+
 getUserById('16t', users, function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address);
 });
 
 
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+function find(arr, cb){
+  for(var i = 0; i < arr.length; i++){
+    if(cb(arr[i])){
+      return arr[i];
+    }
+  }
+}
 
 
-
-
-//Looks through each value in the list, returning the first one that passes a truth test 
+//Looks through each value in the list, returning the first one that passes a truth test
 var numbers  = [1, 2, 3, 4, 5, 6];
-find(numbers, function(num){ 
-  return num % 2 == 0; //should return 2
-})
+find(numbers, function(num){
+  return num % 2 === 0; //should return 2
+});
